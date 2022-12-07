2feea9050db8ff92071ae926c7368af869a41766
wahyu-hadi <102220158+wahyu-hadi@users.noreply.github.com>
2022-12-07 22:15:21 +0700

    Add files via upload

---
+++ Matkul.java
@@ -0,0 +1,11 @@
+public class Matapelajaran {
+    String kdmt;
+
+    String nmmt;
+    final int nilailulus=70;
+
+    void cetakmt(){
+        System.out.println("kode matapelajaran ="+ kdmt + "\n" + "Matapelajaran ="+ nmmt+ "\n"+
+                "Nilai Standar Lulus="+nilailulus);
+    }
+}
---
+++ main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner obj=new Scanner(System.in);
+        Siswa siswa=new Siswa(18210005,"Ilham kusuma jaya");
+        siswa.cetak_siswa();
+        System.out.println("============input data Matapelajaran :======");
+        Nilai nilai=new Nilai();
+        Nilai grade= new Nilai();
+        System.out.print("kode Matapelajaran =");
+        nilai.kdmt= obj.nextLine();
+        System.out.print("nama pelajaran =");
+        nilai.nmmt=obj.nextLine();
+        System.out.println("============Data Matapelajaran============");
+        nilai.cetakmt();
+        System.out.println("============Data Nilai============");
+        System.out.print("jumlah kehadiran =");
+        nilai.nilai_absen=obj.nextInt();
+        System.out.print("Nilai tugas 1=");
+        nilai.nilai_tugas=obj.nextInt();
+        System.out.print("Nilai tugas 2=");
+        nilai.nilai_tugas=obj.nextInt();
+        System.out.print("Nilai tugas 3=");
+        nilai.nilai_tugas=obj.nextInt();
+        System.out.print("nilai_UTS =");
+        nilai.nilai_uts=obj.nextInt();
+        System.out.print("nilai_UAS =");
+        nilai.nilai_uas=obj.nextInt();
+        nilai.addnnlai();
+        System.out.println("============Status Nilai============");
+        nilai.getRata_rata();
+        nilai.cetak_nilai();
+        System.out.println("================================");
+        nilai.cetak_status();
+    }
+}
+}
